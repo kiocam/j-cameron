f561945fe18ebb8f998e9c2e34d216db0d0803c4
Jason Cameron <jasoncameron246@gmail.com>
2020-06-11 19:03:46 -0400

    initial setup

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello from header</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+  }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
